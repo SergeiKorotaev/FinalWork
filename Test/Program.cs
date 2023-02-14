0e2dc0717e67223c859abb67b3241c513e1dcb9b
Sergei Korotaev <serega_korotaev24@gmail.ru>
2023-02-14 18:24:54 +0300

    Ввод данных

--- Test/Program.cs
+++ Test/Program.cs
@@ -7,3 +7,6 @@
     Console.WriteLine();
 }
 
+string[] array1 = new string[5] {"123", "11", "1234", "abcde", "56f"};
+string[] array2 = new string[array1.Length];
+void NewArray(string[] array1, string[] array2)
